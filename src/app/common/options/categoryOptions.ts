1b84c0344b2bc0d1ff2bb3d8c87d687fb2ba4012
kaca306 <katarina.grujic97@gmail.com>
2020-03-26 19:10:06 +0100

    Add additional categories

--- src/app/common/options/categoryOptions.ts
+++ src/app/common/options/categoryOptions.ts
@@ -1,8 +1,11 @@
 export const category = [
-    {key: 'drinks', text: 'Drinks', value: 'drinks'},
-    {key: 'culture', text: 'Culture', value: 'culture'},
-    {key: 'film', text: 'Film', value: 'film'},
-    {key: 'food', text: 'Food', value: 'food'},
-    {key: 'music', text: 'Music', value: 'music'},
-    {key: 'travel', text: 'Travel', value: 'travel'},
+  { key: "drinks", text: "Drinks", value: "drinks" },
+  { key: "culture", text: "Culture", value: "culture" },
+  { key: "film", text: "Film", value: "film" },
+  { key: "food", text: "Food", value: "food" },
+  { key: "music", text: "Music", value: "music" },
+  { key: "travel", text: "Travel", value: "travel" },
+  { key: "art", text: "Art", value: "art" },
+  { key: "technology", text: "Technology", value: "technology" },
+  { key: "science", text: "Science", value: "science" }
 ];
